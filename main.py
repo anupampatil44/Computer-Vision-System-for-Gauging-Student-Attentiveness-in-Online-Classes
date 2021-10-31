eb196597d24f65625860bdcdbb14b75540adf0a7
anupampatil44 <anupampatil44@gmail.com>
2021-10-31 11:33:20 +0530

    minor

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ model_points = np.array([
                             (0.0, 0.0, 0.0),             # Nose tip
                             (0.0, -330.0, -65.0),        # Chin
                             (-225.0, 170.0, -135.0),     # Left eye left corner
-                            (225.0, 170.0, -135.0),      # Right eye right corne
+                            (225.0, 170.0, -135.0),      # Right eye right corner
                             (-150.0, -150.0, -125.0),    # Left Mouth corner
                             (150.0, -150.0, -125.0)      # Right mouth corner
                         ])
